ddbcb8fa16cea402dbb9c9fe4371ca316ec8e2f1
Thanaphat SK <fordthanaphat2006@gmail.com>
2026-08-17 12:54:32 +0700

    docker

--- docker/start-apache.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-APP_PORT="${PORT:-8080}"
-sed -ri "s/^Listen .*/Listen ${APP_PORT}/" /etc/apache2/ports.conf
-sed -ri "s/<VirtualHost \*:[0-9]+>/<VirtualHost *:${APP_PORT}>/" /etc/apache2/sites-available/000-default.conf
-
-exec apache2-foreground
